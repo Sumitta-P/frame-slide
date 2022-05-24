77975f821147412e4dbf0e954ef0e6b2920ac642
sumitta pokaew <sumittapokaew@gmail.com>
2022-05-24 18:47:30 +0700

    first commit

---
+++ src/images.js
@@ -0,0 +1,11 @@
+import image3 from './assets/image3.jpg';
+import image5 from './assets/image5.jpg';
+import image6 from './assets/image6.jpg';
+import image7 from './assets/image7.jpg';
+import image8 from './assets/image8.jpg';
+import image10 from './assets/image10.jpg';
+import image11 from './assets/image11.jpg';
+import image12 from './assets/image12.jpg';
+
+export default [image3, image5, image6, image7,
+                    image8, image10, image11, image12];
